5bbdd3a79f802d06bc90fe1c8c8ebfa878efb7ff
bellaja <bellaja185@gmail.com>
2023-01-21 21:06:41 +0300

    A,B

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,12 @@ public class Main {
         MagicBox<Integer> magicBox = new MagicBox<>(3);
         System.out.println(magicBox.add(1));
         System.out.println(magicBox.add(7));
+        System.out.println(magicBox.add(4));
         System.out.println(magicBox.pick());
+
+        MagicBox<String> magicBox1 = new MagicBox<>(2);
+        System.out.println(magicBox1.add("A"));
+        System.out.println(magicBox1.add("B"));
+        System.out.println(magicBox1.pick());
     }
 }
